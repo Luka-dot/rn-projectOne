085702939fd53a6c57b5ba470e051116703f7fac
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-05-29 17:04:31 -0400

    refactor to separate componenets, adding and deleting functions added

--- App.js
+++ App.js
@@ -1,19 +1,49 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
+
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const addGoalHandler = (goalTitle) => {
+    setCourseGoals(currentGoals => [...currentGoals, { id: Math.random().toString(), value: goalTitle}]);
+  };
+
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.id !== goalId); // only keeping IDs that doesnt match goalId
+    });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>      
+      <GoalInput onAddGoal={addGoalHandler} />
+        <FlatList data={courseGoals} renderItem={itemData => (
+          <GoalItem 
+            title={itemData.item.value} 
+            id={itemData.item.id} 
+            onDelete={removeGoalHandler} />
+      )} >          
+      </FlatList>      
     </View>
   );
 }
 
+// styles.container
 const styles = StyleSheet.create({
   container: {
+    padding: 30,
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'indigo',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  screen: {
+    backgroundColor: 'indigo',
+    padding: 50,
+    flex: 1
+  }
+  
 });
